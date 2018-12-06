b7ae6ad77c9db95ead65c9aac113425fb8095fca
guillaumesibot <guillaumesibot@gmail.com>
2018-12-06 15:48:33 +0100

    Using FRotator In Unreal

--- Source/Battle_Tank/Private/TankAimingComponent.cpp
+++ Source/Battle_Tank/Private/TankAimingComponent.cpp
@@ -19,51 +19,44 @@ void UTankAimingComponent::SetBarrelReference(UStaticMeshComponent* BarrelToSet)
 	this->Barrel = BarrelToSet;
 }
 
-
-// Called when the game starts
-void UTankAimingComponent::BeginPlay()
-{
-	Super::BeginPlay();
-
-	// ...
-	
-}
-
-
-// Called every frame
-void UTankAimingComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
-}
-
 void UTankAimingComponent::AimAt(FVector HitLocation,float LaunchSpeed)
 {
 	if (!Barrel) { return; }
 
 	FVector OutLaunchVelocity(0);
 	FVector StartLocation = Barrel->GetSocketLocation(FName("Projectile"));
-	if (UGameplayStatics::SuggestProjectileVelocity
+	bool bHaveAimSolution = UGameplayStatics::SuggestProjectileVelocity
 	(
 		this,
 		OutLaunchVelocity,
 		StartLocation,
 		HitLocation,
 		LaunchSpeed,
-		false,
-		0,
-		0,
 		ESuggestProjVelocityTraceOption::DoNotTrace
-		
-	)
-		)
+		);
+		if (bHaveAimSolution)
 	{
 		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
-		UE_LOG(LogTemp, Warning, TEXT("Aiming at %s"),*AimDirection.ToString());
+		
+		MoveBarrelTowards(AimDirection);
 	}
 	//if no solution do nothing		UE_LOG(LogTemp, Warning, TEXT("not solution"));
 	
 }
 
 
+void  UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
+{
+	// Work-out difference between current barrel roation, and AimDirection
+
+	auto BarrelRotator = Barrel->GetForwardVector().Rotation();
+	auto AimAsRotator = AimDirection.Rotation();
+	auto DeltaRotator = AimAsRotator - BarrelRotator;
+	UE_LOG(LogTemp, Warning, TEXT("AimAsRotator: %s"), *DeltaRotator.ToString());
+
+	// Move the barrel the right amount this frame
+
+	// Given a max elevation speed, and the frame time
+
+
+}
--- Source/Battle_Tank/Public/TankAimingComponent.h
+++ Source/Battle_Tank/Public/TankAimingComponent.h
@@ -19,17 +19,11 @@ public:
 
 	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
 
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	//TODO add SetTurretReference
 
 	void AimAt(FVector HitLocation, float LunchSpeed);
 private: 
 	class UStaticMeshComponent* Barrel = nullptr;
 
-
+	void  MoveBarrelTowards(FVector AimDirection);
 };
